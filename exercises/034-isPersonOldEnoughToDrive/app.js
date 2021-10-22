dd415e1f40828a3c4450429b317568aaba620e30
paola9896 <paolasc2652@gmail.com>
2021-10-22 22:33:15 +0000

    first commit

--- exercises/034-isPersonOldEnoughToDrive/app.js
+++ exercises/034-isPersonOldEnoughToDrive/app.js
@@ -1,3 +1,13 @@
 function isPersonOldEnoughToDrive(person) {
-  // tu codigo aqui
+  if (person.age >= 16) {
+    return true;
+} else {
+    return false;
 }
+}
+
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrive(obj);
+console.log(output); // --> true
--- exercises/035-isPersonOldEnoughToVote/app.js
+++ exercises/035-isPersonOldEnoughToVote/app.js
@@ -1,3 +1,13 @@
 function isPersonOldEnoughToVote(person) {
-  // tu codigo aqui
+  if (person.age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+var obj = {
+  age: 19
+};
+var output = isPersonOldEnoughToVote(obj);
+console.log(output); // --> true
